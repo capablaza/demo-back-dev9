b20fd867c8aa3db909f9a53bf3ad01b4f4fcd4b9
carlos apablaza <c.apablaza.z@gmail.com>
2021-08-18 10:08:21 -0400

    add second rules

--- src/test/java/com/demo/credit/creditSimulator/consumer/CreditConsumerTest.java
+++ src/test/java/com/demo/credit/creditSimulator/consumer/CreditConsumerTest.java
@@ -43,7 +43,7 @@ public class CreditConsumerTest {
         //check evaluation that's correct
         assertThat(answer).isTrue();
     }
-/*
+
     @Test
     public void test2() {
         //check second range test case
@@ -57,7 +57,7 @@ public class CreditConsumerTest {
         //check evaluation that's correct
         assertThat(consumer.evaluate()).isTrue();
     }
-
+/*
     @Test
     public void test3() {
         //check third range test case
@@ -88,7 +88,7 @@ public class CreditConsumerTest {
         assertThat(consumer.evaluate()).isFalse();
     }
 
-/*
+
     @Test
     public void test5() {
         //check second range test case
@@ -102,7 +102,7 @@ public class CreditConsumerTest {
         //check evaluation that's correct
         assertThat(consumer.evaluate()).isFalse();
     }
-
+/*
     @Test
     public void test6() {
         //check third range test case
